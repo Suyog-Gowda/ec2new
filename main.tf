675aea81969bc5aa3f5a55a3330b3eee6561a68e
Suyog-Gowda <106002049+Suyog-Gowda@users.noreply.github.com>
2022-10-28 11:19:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
+terraform {
+	backend "s3" {
+		bucket = "suyogs31990"
+		key = "terraform/remotestate/terraform.tfstate"
+		region = "ap-south-1"
+		
+		dynamodb_table = "suyogdb"
+		encrypt = true
+	}
+}
+
+provider "aws" {
+	region = "ap-south-1"
+}
+
 resource "aws_instance" "instance-ec2" {
 	ami = "ami-05c8ca4485f8b138a"
 	instance_type = "t2.micro"
 	key_name = "Suyog"
 	tags = {
-			Name = "Trial_instance"
+	    Name = "Trial_instance"
 	}
-}
-terraform {
-  backend "local" {
-     path = "terraform.tfstate.d/dev"
-  }
 }
